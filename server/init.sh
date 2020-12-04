1ef623780c325160ec8d89eae1d0190368d1ec1b
Guillaume ROZIER <guillaume.rozier@telecomnancy.net>
2020-12-04 11:45:38 +0100

    update server init

--- server/init.sh
+++ server/init.sh
@@ -5,7 +5,7 @@ do
 	
 	echo start_script
 	sudo git fetch --all
-	sudo git reset --hard origin/master && sudo jupyter nbconvert --to python *.ipynb src/france/*.ipynb src/world/*.ipynb
+	sudo git reset --hard origin/master && sudo jupyter nbconvert --to python server/*.ipynb src/france/*.ipynb src/world/*.ipynb
 	sudo python3 server/script_update_data.py
 
 	sleep 60
